444f7243e3e733c485e141d136e87cb9804debca
Vamsi Peddi <vamsik.peddi@gmail.com>
2025-06-06 18:30:37 -0500

    Config

--- ifl-user-auth/src/main/java/com/simplytrack/strack_user_auth/StrackUserAuthApplication.java
+++ ifl-user-auth/src/main/java/com/simplytrack/strack_user_auth/StrackUserAuthApplication.java
@@ -2,10 +2,8 @@ package com.simplytrack.strack_user_auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class StrackUserAuthApplication {
 
 	public static void main(String[] args) {
